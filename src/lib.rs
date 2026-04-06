93846f3f044ce8e6ce666be14f2e77037ce896c4
shigedangao <marc.inthaamnouay@gmail.com>
2026-04-06 21:57:23 +0200

    improve brick documentation

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,47 @@ use item::ProcessItem;
 
 /// Brick proc macro is a macro which generates a struct or enum with the specified attributes.
 /// This allows to convert a struct to another struct which may contains similar fields while also
-/// enabled to convert a field to another type or rename that field.
+/// enabled to convert a field to another type or rename that field. Below are some examples below. Although more detailed examples can be found on the [README](https://github.com/shigedangao/brick)
 ///
 /// # Examples
+///
+/// ## Map a struct to another struct
+///
+/// ```
+/// use brick::brick;
+///
 /// struct Foo {
 ///     a: i32,
 ///     b: String,
 /// }
 ///
-/// #[brick(operator="From", source="Foo")]
+/// #[brick(converter="From", source="Foo")]
 /// struct Bar {
 ///     a: i32,
 ///     #[brick_field(rename="b")]
 ///     c: String,
 /// }
+/// ```
+///
+/// ## Map a struct with a transform function
+///
+/// ```
+/// use brick::brick;
+///
+/// struct Bar {
+///     a: i32
+/// }
+///
+/// fn add_self(a: i32) -> i32 {
+///     a + a
+/// }
+///
+/// #[brick(converter="From", source="Bar")]
+/// struct Target {
+///     #[brick_field(transform_fn="add_self")]
+///     a: i32,
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn brick(args: TokenStream, target: TokenStream) -> TokenStream {
     let input_kind = parse_macro_input!(target as Item);
